079097df58feeabeafd41bad324aa25cd2eabed1
Abhay_CH <abhaychandavar11@gmail.com>
2020-03-30 14:11:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import Weather from "./components/weather";
+import { Helmet } from "react-helmet";
+const TITLE = "Weather";
 class App extends Component {
   render() {
     return (
       <div>
+        <Helmet>
+          <title>{TITLE}</title>
+        </Helmet>
         <Weather />
       </div>
     );
